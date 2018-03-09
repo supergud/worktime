b7498ca3075072a242e31b7219d3a086e73be818
Bruce Lee <supergud3@gmail.com>
2018-03-10 01:36:26 +0800

    paser holiday data and write time

--- worktime.py
+++ worktime.py
@@ -1,9 +1,18 @@
 # !/usr/bin/python
 # coding:utf-8
 # xlsxwriter的基本用法
-import xlsxwriter
-import datetime, calendar
+import xlsxwriter, datetime, calendar, requests, json
 import sys
+
+url = "http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000077-002"
+response = requests.get(url)
+data = json.loads(response.content)
+
+if data['success']:
+    result = data['result']
+    records = result['records']
+else:
+    records = []
 
 def weekday(x):
     return {
@@ -21,6 +30,11 @@ def format(custom_format, need_center, need_border):
     border = {'border': 1} if need_border else {}
     format_result = {**custom_format, **center, **border}
     return workbook.add_format(format_result)
+
+def get_holiday_name(date):
+    for record in records:
+        if record['date'] == date:
+            return record['name']
 
 year = 2018
 month = 1
@@ -62,12 +76,30 @@ for row in range(3, 19):
         worksheet.write(chr(col) + str(row), '', default_format)
 
 for index, day in enumerate(days):
+    date = str(year) + '/' + day['day']
+    holiday_name = get_holiday_name(date)
+    num = str(index % 16 + 3)
+
     if index < 16:
-        worksheet.write("A" + str(index % 16 + 3), day['day'], default_format)
-        worksheet.write("B" + str(index % 16 + 3), weekday(day['weekday']), default_format)
+        worksheet.write("A" + num, day['day'], default_format)
+        worksheet.write("B" + num, weekday(day['weekday']), default_format)
+        if holiday_name is None:
+            worksheet.write("C" + num, '9:00', default_format)
+            worksheet.write("D" + num, '18:00', default_format)
+            worksheet.write("E" + num, '8', default_format)
+            worksheet.write("F" + num, '0', default_format)
+        else:
+            worksheet.write("G" + num, holiday_name, default_format)
     else:
-        worksheet.write("H" + str(index % 16 + 3), day['day'], default_format)
-        worksheet.write("I" + str(index % 16 + 3), weekday(day['weekday']), default_format)
+        worksheet.write("H" + num, day['day'], default_format)
+        worksheet.write("I" + num, weekday(day['weekday']), default_format)
+        if holiday_name is None:
+            worksheet.write("J" + num, '9:00', default_format)
+            worksheet.write("K" + num, '18:00', default_format)
+            worksheet.write("L" + num, '8', default_format)
+            worksheet.write("M" + num, '0', default_format)
+        else:
+            worksheet.write("N" + num, holiday_name, default_format)
 
 worksheet.merge_range('A19:J19', "※每日工作9小時，中午休息一個小時，共為8小時。\n※延長工作時數：每日不得超過12小時，每月不得超過46小時。\n※出勤紀錄，應逐日記載勞工出勤情形至分鐘為止。依據勞動基準法第 30條規定，應保存五年。", text_wrap)   
 worksheet.write('K19', '簽名', default_format)
@@ -77,6 +109,16 @@ worksheet.set_default_row(25)
 worksheet.set_row(0, 35)
 worksheet.set_row(1, 20)
 worksheet.set_row(18, 50)
+worksheet.set_column('A:A', 5)
+worksheet.set_column('H:H', 5)
+worksheet.set_column('B:B', 6)
+worksheet.set_column('I:I', 6)
+worksheet.set_column('C:D', 8)
+worksheet.set_column('J:K', 8)
+worksheet.set_column('E:F', 10)
+worksheet.set_column('L:M', 10)
+worksheet.set_column('G:G', 25)
+worksheet.set_column('N:N', 25)
 
 # 5.7 關閉並儲存文件
 workbook.close()
