8d8c4c2bb31706810b9612a3b10023f6d9c0a27f
Bruce Lee <supergud3@gmail.com>
2018-03-09 23:18:40 +0800

    add weekday and update year and month to title

--- worktime.py
+++ worktime.py
@@ -5,10 +5,21 @@ import xlsxwriter
 import datetime, calendar
 import sys
 
+def weekday(x):
+    return {
+        0: "週一",
+        1: "週二",
+        2: "週三",
+        3: "週四",
+        4: "週五",
+        5: "週六",
+        6: "週日"
+    }[x]
+
 year = 2018
 month = 1
 num_days = calendar.monthrange(year, month)[1]
-days = [str(month) + '/' + str(day) for day in range(1, num_days + 1)]
+days = [{'day' : str(month) + '/' + str(day), 'weekday' : (datetime.datetime(year, month, day).weekday())} for day in range(1, num_days + 1)]
 
 # 1. 建立一個Excel文件
 workbook = xlsxwriter.Workbook('demo1.xlsx')
@@ -22,7 +33,7 @@ bold = workbook.add_format({'bold':True})
 # 5. 向單元格寫入資料
 # 5.1 向A1單元格寫入'Hello'
 worksheet.write('A1', '員工編號：',bold)
-worksheet.write('A2', '諸度股份有限公司 工時紀錄   民國            年            月份     員工：   ',bold)
+worksheet.write('A2', '諸度股份有限公司 工時紀錄   民國 ' + str(year - 1911) + ' 年 ' + str(month) + ' 月份     員工：   ', bold)
 worksheet.write('A3', '日期')
 worksheet.write('B3', '星期')
 worksheet.write('C3', '上班')
@@ -39,7 +50,8 @@ worksheet.write('M3', '加班時數')
 worksheet.write('N3', '備註')
 
 for index, day in enumerate(days):
-    worksheet.write(("A" if index < 16 else "H") + str(index % 16 + 4), day)
-
+    worksheet.write(("A" if index < 16 else "H") + str(index % 16 + 4), day['day'])
+    worksheet.write(("B" if index < 16 else "H") + str(index % 16 + 4), weekday(day['weekday']))
+    
 # 5.7 關閉並儲存文件
 workbook.close()
